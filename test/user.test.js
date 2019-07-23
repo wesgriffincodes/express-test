7e3eb10b0b3991aa0751d4235bc79603929a36a6
Wes Griffin <codingclueless@gmail.com>
2019-07-23 12:34:20 -0700

    test pass

--- test/user.test.js
+++ test/user.test.js
@@ -50,11 +50,36 @@ describe('user routes', () => {
       .then(res => {
         const usersJSON = JSON.parse(JSON.stringify(users));
         usersJSON.forEach(user => {
-          expect(res.body).toContainEqual({ name: user.name, id: user._id });
+          expect(res.body).toContainEqual({ name: user.name, _id: user._id });
         });
       });
   });
 
-  
+  it('GET user by ID', async() => {
+    return request(app)
+      .get(`/api/v1/users/${user._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          name: user.name,
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can update a user by id', async() => {
+
+    return request(app)
+      .put(`/api/v1/users/${user._id}`)
+      .send({ name: 'Billy', age: 20 })
+      .then(res => {
+        const userJSON = JSON.parse(JSON.stringify(user));
+        expect(res.body).toEqual({
+          ...userJSON,
+          name: 'Billy',
+          age: '20'
+        });
+      });
+  });
+
 
 });
